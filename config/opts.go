35b31d0380517ffa69438a9d06501606da124832
Markus Blaschke <mblaschke82@gmail.com>
2020-07-16 15:36:40 +0200

    Fix typo in NOTIFICATION env name
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- config/opts.go
+++ config/opts.go
@@ -48,7 +48,7 @@ type (
 		Drain OptsDrain
 
 		// notification
-		Notification []string `long:"notification" env:"NOTIFCATION" description:"Shoutrrr url for notifications (https://containrrr.github.io/shoutrrr/)" env-delim:" "`
+		Notification []string `long:"notification" env:"NOTIFICATION" description:"Shoutrrr url for notifications (https://containrrr.github.io/shoutrrr/)" env-delim:" "`
 
 		// server settings
 		ServerBind string `long:"bind" env:"SERVER_BIND"  description:"Server address"  default:":8080"`
